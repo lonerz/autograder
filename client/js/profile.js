0baae60b335de58aada8f482b08ed7c87f96e1bf
Cole <coler706@gmail.com>
2018-04-27 08:25:34 -0700

    Nicer Assignments

--- client/js/profile.js
+++ client/js/profile.js
@@ -1,8 +1,27 @@
-function setAssignments(asgn) {
-    var assgn = JSON.parse(asgn);
+function htmlForAssignment(assgn){
+  var s=`<div class="row">
+    <div class="col s12 m12">
+      <div class="card">
+        <div class="card-content">
+          <span class="card-title">`+assgn+`</span>
+        </div>
+        <div class="card-action">
+          <a href ="/upload/` + assgn + `">Submit</a>
+          <a href ="/results/` + assgn + `">Results</a>
+        </div>
+      </div>
+    </div>
+  </div>`;
+  return s;
+
+}
+function setAssignments(asgnresults) {
+    var assgn = JSON.parse(asgnresults);
     console.log(assgn);
     for(var i in assgn) {
-        $("#assignments").append("<b>" +assgn[i] + ": </b>" + "<a href='/upload/" + assgn[i] + "'>submit</a> / <a href ='/results/" + assgn[i] + "'>results</a> <br>");
+      var htmlValue=htmlForAssignment(assgn[i]);
+      //var htmlValue='<li class="collection-item"><span class="title">'+assgn[i]+'</span><p><a href="/upload/' + assgn[i] + '">submit</a><br><a href ="/results/' + assgn[i] + '">results</a></p></li>';
+        $("#assignments").append(htmlValue);
     }
 }
 
