85dec2d81cf459787c6c8fe1979a6aa058beae25
Joshua Pan <joshuapan8@gmail.com>
2018-05-10 00:56:59 -0700

    Make gradebook table include testcases.

--- client/js/gradebook.js
+++ client/js/gradebook.js
@@ -1,6 +1,20 @@
+var num_tests = {
+    'Stuffie': 9,
+    'Flashlight': 4,
+};
+
+
 function students(obj) {
     obj = JSON.parse(obj);
     console.log(obj);
+    createCols(obj);
+}
+
+function createCols(obj) {
+    var asgn = $("#asgn").text();
+    for(var i=1; i<=num_tests[asgn]; ++i) {
+        $("#col").append("<th>T"+i+"</th>");
+    }
     createTable(obj);
 }
 
@@ -20,19 +34,20 @@ function createRow(obj) {
     if(obj[0]) {
         score = obj[0].scores || obj[0].score;
     }
-    $("#add").append("<tr><td>" + name + "</td><td>" + score + "</td></tr>");
-}
-
-function setResults(obj) {
-    obj = JSON.parse(obj);
-    console.log(obj);
-    var date = new Date(obj[1].submitted);
-    $("#results").append("Job ID: <b>" + obj[1]._id + "</b>, submitted at " + date.toGMTString() + ". <br>");
-    if(obj[0]) $("#results").append("<b> Most recent score: </b>" + obj[0].score + ". <br>");
+    obj = obj[0];
+    var asgn = $("#asgn").text();
+    var s = "<td>" + name + "</td><td>" + score + "</td>";
+    for(var i=1; i<=num_tests[asgn]; ++i) {
+        if(obj.testcases && obj.testcases[i]) {
+            s += "<td>"+obj.testcases[i].score+"</td>";
+        } else {
+            s += "<td></td>";
+        }
+    }
+    $("#row").append("<tr>" + s + "</tr>");
 }
 
 window.onload = function() {
     GET("../students", students);
-    // GET("../recent/" + $("#asgn").text(), setResults);
 };
 
