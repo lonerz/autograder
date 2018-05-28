dff943b39f8edaac79618848ae43aa8fd0691791
Joshua Pan <joshuapan8@gmail.com>
2018-05-28 03:53:43 -0700

    Allow clicking on submissions in queue.

--- client/js/queue.js
+++ client/js/queue.js
@@ -1,24 +1,49 @@
 function htmlForSubmission(sub){
   var date = new Date(sub.submitted);
-  var s=
-  `<div class="row" style="margin: 0;">
-    <div class="col s12 m12">
-      <div class="card horizontal">
-        <div class="card-image valign-wrapper">`
-          + (sub.graded ? `<i class="teal-text material-icons small">check_box</i>` : `<i class="red-text material-icons small">clear</i>`) +
-        `</div>
-        <div class="card-stacked">
-          <div class="card-content">
-            <div class="row" style="margin: 0;">
-              <div class="col s3 center-align">`+sub.username+`</div>
-              <div class="col s4 center-align">`+sub.asgn+`</div>
-              <div class="col s5 center-align"><i class="material-icons tiny">access_time</i> ` + date.toGMTString() + `</div>
+  var username = $("#email").text().split(" ")[1].split("@")[0];
+  var s = '';
+  console.log(username);
+  if(sub.username !== username) {
+      s=
+      `<div class="row" style="margin: 0;">
+        <div class="col s12 m12">
+          <div class="card horizontal">
+            <div class="card-image valign-wrapper">`
+              + (sub.graded ? `<i class="teal-text material-icons small">check_box</i>` : `<i class="red-text material-icons small">clear</i>`) +
+            `</div>
+            <div class="card-stacked">
+              <div class="card-content">
+                <div class="row" style="margin: 0;">
+                  <div class="col s3 center-align">`+sub.username+`</div>
+                  <div class="col s4 center-align">`+sub.asgn+`</div>
+                  <div class="col s5 center-align"><i class="material-icons tiny">access_time</i> ` + date.toGMTString() + `</div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
-  </div>`;
+      </div>`;
+  } else {
+      s=
+      `<div class="row" style="margin: 0;">
+        <div class="col s12 m12">
+          <div class="card horizontal yellow lighten-3">
+            <div class="card-image valign-wrapper">`
+              + (sub.graded ? `<i class="teal-text material-icons small">check_box</i>` : `<i class="red-text material-icons small">clear</i>`) +
+            `</div>
+            <div class="card-stacked">
+              <div class="card-content mycard">
+                <div class="row" style="margin: 0;">
+                  <div class="col s3 center-align">`+sub.username+`</div>
+                  <div class="col s4 center-align asgn">`+sub.asgn+`</div>
+                  <div class="col s5 center-align"><i class="material-icons tiny">access_time</i> ` + date.toGMTString() + `</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`;
+  }
   return s;
 };
 
@@ -27,16 +52,23 @@ function setSubmissions(sub) {
     console.log(subs);
     for(var i in subs) {
         console.log(subs[i]);
-        /*
-        var date = new Date(subs[i].submitted);
-        $("#submissions").append("Job ID: <b>" + subs[i]._id + "</b>");
-        $("#submissions").append("<li> name: " + subs[i].username + "</li>");
-        $("#submissions").append("<li> assignment: " + subs[i].asgn + "</li>");
-        $("#submissions").append("<li> graded: " + subs[i].graded + "</li>");
-        $("#submissions").append("<li> submission time: " + date.toGMTString() + "</li>");
-        */
         $("#submissions").append(htmlForSubmission(subs[i]));
     }
+
+    $(".mycard").click(function() {
+        window.location = "/results/" + $(this).find('.asgn').text();
+        console.log($(this).find('.asgn').text());
+    });
+
+    $(".mycard").hover(function() {
+        console.log($(this).closest(".card"));
+        $(this).closest(".card").removeClass("lighten-3");
+        $(this).closest(".card").addClass("lighten-4");
+        $(this).closest(".card").css("cursor", "pointer");
+    }, function() {
+        $(this).closest(".card").removeClass("lighten-4");
+        $(this).closest(".card").addClass("lighten-3");
+    });
 }
 
 window.onload = function() {
